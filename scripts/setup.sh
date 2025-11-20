a845201e75369b9eaefb96b6d8a0b0a0f32e82e9
hyperoot <rajesh@hyperoot.dev>
2025-11-20 17:16:42 +0000

    chore: update project name and setup script paths

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/mise trust /workspaces/devcontainer-astro-template/mise.toml && /usr/local/bin/mise install
+/usr/local/bin/mise trust /workspaces/cloudchallenges/mise.toml && /usr/local/bin/mise install
 sudo apt update && sudo apt install -y tmux python3
