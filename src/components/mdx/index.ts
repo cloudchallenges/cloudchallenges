e068afb5c2589c7da63a38c7adbfa9283d032cf5
hyperoot <rajesh@hyperoot.dev>
2025-12-05 02:50:34 +0000

    feat(mdx): add MdxImage and MdxLink components for MDX support

---
+++ src/components/mdx/index.ts
@@ -0,0 +1,9 @@
+/**
+ * MDX Component Overrides
+ *
+ * This module exports custom Astro components that override
+ * default HTML elements when rendering MDX/Markdown content.
+ */
+
+export { default as MdxImage } from "./MdxImage.astro";
+export { default as MdxLink } from "./MdxLink.astro";
